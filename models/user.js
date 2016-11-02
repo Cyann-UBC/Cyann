9cea7625aa52504dbe2df63f7ce2887232404703
Justin Toh <tohjustin@hotmail.com>
2016-11-02 11:38:31 -0700

    Refactored Models for consistency

--- models/user.js
+++ models/user.js
@@ -1,64 +1,54 @@
 "use strict"
-
 var mongoose = require('mongoose');
-var Schema =   mongoose.Schema; // create instance of Schema
-var forumPosts  =   require("./mongo");
 var bcrypt = require('bcrypt');
 
-var db = mongoose.createConnection('mongodb://localhost:27017/CyannUser');
+var db = mongoose.createConnection('mongodb://localhost:27017/userCollection');
+    db.on('error',function(err){ console.error("MongoDB Connection Failed",err); });
+    db.once("open",function(){ console.log("MongoDB [userCollection] connected successfully"); });
 
-db.on('error',function(err){
-  console.error("connection error",err);
+var UserSchema = new mongoose.Schema({
+    "username": { type: String, unique: true, required: true, trim: true },
+    "name": { type: String, required: true },
+    "password": { type: String, required: true, select: false },
+    "userType": { type: String, required: true },
+    "honour": { type: Number, default: 0 },
+    "joined": { type: Date, default: Date.now , select: false},
+    "posts": [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course.posts' }],
+    "comments": [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course.posts.comments '}]
 });
 
-db.once("open",function(){
-  console.log("CyanUser connection successful");
-});
-
-
-var UserSchema = new Schema({
-    "ssc" : {type:String, unique:true, required:true, trim:true},
-    "name": {type:String, required:true},
-    "password"  : {type:String, required:true},
-    "userType"  : {type:String, required:true},
-    "honor"    : Number,
-    "joined" : { type: Date, default: Date.now },
-    "posts"   : [{type: mongoose.Schema.Types.ObjectId, ref: 'course.posts'}],
-    "comments":[{type: mongoose.Schema.Types.ObjectId, ref: 'course.comments'}],
-});
-
-UserSchema.statics.authenticate = function(ssc, password, callback){
-  User.findOne({ssc:ssc})
-    .exec(function(error, user){
-      if(error){
-        return callback(error);
-      }else if(!user){
-        var err = new Error('User not found');
-        err.status = 401
-        return callback(err)
-      }
-      bcrypt.compare(password, user.password, function(error, result){
-        if(result === true){
-           console.log("asd")
-          return callback(null, user)
-        }else{
-          return callback()
-        }
-      })
-    })
+UserSchema.statics.authenticate = function(username, password, callback){
+    User.findOne({username:username})
+        .exec(function(error, user){
+            if(error){
+                return callback(error);
+            }else if(!user){
+                var err = new Error('User not found');
+                err.status = 401
+                return callback(err)
+            }
+            bcrypt.compare(password, user.password, function(error, result){
+                if(result === true){
+                    console.log("asd")
+                    return callback(null, user)
+                }else{
+                    return callback()
+                }
+            })
+        })
 }
 
 UserSchema.pre('save', function(next) {
-  var user = this;
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
 });
 
-
-var User = db.model('User', UserSchema);
-module.exports = User;
+// argv[0] == db collection name,
+// argv[1] == mongoose schema to use
+module.exports = db.model('User', UserSchema);
