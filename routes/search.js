3743b7be8f8377d025674df75959f49fb10768b4
Justin Toh <tohjustin@hotmail.com>
2016-11-13 17:58:06 -0800

    Implemented SEARCH feature by KEYWORD & USER_ID

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,42 @@
 var Courses = require("../models/course.js");
 var Users = require("../models/user.js");
 
-// Search posts
+// Search posts by:
+// - Keyword
+// - Author (userId)
+// - Timestamp ()
 exports.searchPosts = function(req,res){
-    res.json({ message: "Search works!" });
-}
+
+    var keyword = req.query.keyword;
+    var userId = req.query.userId;
+
+    Courses.findById({ '_id': req.params.courseId })
+        .select("-__v")
+        .populate("instructor", "name", Users)
+        .populate("posts.author", "name", Users)
+        .populate("posts.comments.author", "name", Users)
+        .then(function (result){
+            var filteredResults = result.posts;
+
+            // FILTER out posts whose TITLE or CONTENT doesn't contain the keyword
+            if( keyword ){
+                filteredResults = filteredResults.filter(function(thisPost){
+                    return thisPost.title.includes(keyword) || thisPost.content.includes(keyword);
+                });
+            }
+
+            // FILTER out posts that isn't author'ed by the given userId
+            if( userId ){
+                res.json({ message: 'BbBBB!', data: filteredResults });
+                filteredResults = filteredResults.filter(function(thisPost){
+                    return String(thisPost.author._id) === String(userId);
+                });
+            }
+
+            res.json({ message: 'Retrieved posts from search query!', data: filteredResults });
+
+        })
+        .catch(function(err){
+            res.send(err);
+        });
+};
