7522529736e91d7e5c26f458ca24331a9d3e3af6
Justin Toh <tohjustin@hotmail.com>
2016-11-16 14:20:28 -0800

    Completed findCommentById()

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -22,69 +22,62 @@ exports.findById = function(req,res){
 exports.findPostById = function(req,res){
 
     var userId = req.params.userId;
-    Courses.aggregate(  [   {  $match: {'posts.author': mongoose.Types.ObjectId(userId)} },
-                            {  $unwind: '$posts' },
-                            { $project: {   _id: 0,
-                                            'courseId': '$_id',
-                                            'postId': '$posts._id', 
-                                            'title': '$posts.title', 
-                                            'content': '$posts.content', 
-                                            'author': '$posts.author', 
-                                            'course': '$posts.course', 
-                                            'updatedAt': '$posts.updatedAt', 
-                                            'createdAt': '$posts.createdAt' } }
-                        ], function(err, aggregateResult) {
-                            if (err) {
-                                res.status(400);
-                                res.json(err);
+    Courses.aggregate([ { $match: {'posts.author': mongoose.Types.ObjectId(userId)} },
+                        { $unwind: '$posts' },
+                        { $project: {   _id: 0,
+                                        'course': { 
+                                            '_id': '$_id', 
+                                            'name': '$courseName' 
+                                        },
+                                        '_id': '$posts._id', 
+                                        'title': '$posts.title', 
+                                        'content': '$posts.content', 
+                                        'updatedAt': '$posts.updatedAt', 
+                                        'createdAt': '$posts.createdAt' 
+                                    } },
+                        { $sort: { 'createdAt': -1 } } ], function(err, result) {
+                                if (err) {
+                                    res.status(400);
+                                    res.json(err);
+                                }
+                                else{
+                                    res.status(200);
+                                    res.json(result);
+                                }
                             }
-                            else{
-                                res.status(200);
-                                res.json(aggregateResult);
-                        }
-    });
-
-    // // Alternative (UGLY) solution
-    // Users.findOne({ _id: userId})
-    //     .then(function(userObject){
-    //         postIdArray = userObject.posts;
-    //         return Courses.find({ "posts.author": userId })
-    //     })
-    //     .then(function(courseObj){
-    //         // Extract posts obj from the array courses that have user's posts
-    //         var result = courseObj.map(function(e){
-    //             return e.posts;
-    //         })
-    //         // Flatten array
-    //         result = [].concat.apply([], result);
-    //         // Extract posts made by user
-    //         result = result.filter(function(e){
-    //             return postIdArray.indexOf(e._id) > -1;
-    //         });
-    //         res.json(result);
-    //     })
-    //     .catch(function(err){
-    //         res.status(400);
-    //         res.send(err);
-    //     });
+    );
 }
 
 exports.findCommentById = function(req,res){
-    var promise = Course.aggregate({
-            $match: {'posts.comments.author': {$gte: 'Howard Zhou'}}
-        }, {
-            $unwind: '$posts.comments'
-         }, {
-            $match: {'posts.comments.author': {$gte: 'Howard Zhou'}}
-        }, {
-        $project: {
-            title: '$comments.title',
-            content:'$comments.content'
-        }
-    })
-    promise.then(function(result){
-        console.log(result)
-    }).catch(function(err){
-        res.send(err)
-    });
+    
+    var userId = req.params.userId;
+    Courses.aggregate([ { $match: {'posts.comments.author': mongoose.Types.ObjectId(userId)} },
+                        { $unwind: '$posts' },
+                        { $unwind: '$posts.comments' },
+                        { $match: {'posts.comments.author': mongoose.Types.ObjectId(userId)} }, // match again...
+                        { $project: {   _id: 0,
+                                        '_id': '$posts.comments._id', 
+                                        'course': { 
+                                            '_id': '$_id', 
+                                            'name': '$courseName' 
+                                         }, 
+                                        'post': { 
+                                            '_id': '$posts._id'
+                                         },
+                                        'content': '$posts.comments.content',
+                                        'upvotes': '$posts.comments.upvotes',
+                                        'updatedAt': '$posts.comments.updatedAt', 
+                                        'createdAt': '$posts.comments.createdAt' 
+                                    } },
+                        { $sort: { 'createdAt': -1 } } ], function(err, result) {
+                                if (err) {
+                                    res.status(400);
+                                    res.json(err);
+                                }
+                                else{
+                                    res.status(200);
+                                    res.json(result);
+                                }
+                            }
+    );
 }
