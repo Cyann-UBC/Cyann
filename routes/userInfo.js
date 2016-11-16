7a8e0fad909766a43e5dd449a0ea58854e7d4e6e
Justin Toh <tohjustin@hotmail.com>
2016-11-16 14:29:39 -0800

    Changed implementation to use aggregation pipeline format

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -22,62 +22,66 @@ exports.findById = function(req,res){
 exports.findPostById = function(req,res){
 
     var userId = req.params.userId;
-    Courses.aggregate([ { $match: {'posts.author': mongoose.Types.ObjectId(userId)} },
-                        { $unwind: '$posts' },
-                        { $project: {   _id: 0,
-                                        'course': { 
-                                            '_id': '$_id', 
-                                            'name': '$courseName' 
-                                        },
-                                        '_id': '$posts._id', 
-                                        'title': '$posts.title', 
-                                        'content': '$posts.content', 
-                                        'updatedAt': '$posts.updatedAt', 
-                                        'createdAt': '$posts.createdAt' 
-                                    } },
-                        { $sort: { 'createdAt': -1 } } ], function(err, result) {
-                                if (err) {
-                                    res.status(400);
-                                    res.json(err);
-                                }
-                                else{
-                                    res.status(200);
-                                    res.json(result);
-                                }
-                            }
-    );
+    Courses.aggregate()
+        .match({ 'posts.author': mongoose.Types.ObjectId(userId) })
+        .unwind('$posts')
+        .project({  
+            _id: 0,
+            'course': { 
+                '_id': '$_id', 
+                'name': '$courseName' 
+            },
+            '_id': '$posts._id', 
+            'title': '$posts.title', 
+            'content': '$posts.content', 
+            'updatedAt': '$posts.updatedAt', 
+            'createdAt': '$posts.createdAt' 
+        })
+        .sort({ 'createdAt': -1 })
+        .exec(function(err, result){
+            if (err) {
+                res.status(400);
+                res.json(err);
+            }
+            else{
+                res.status(200);
+                res.json(result);
+            }
+        });
 }
 
 exports.findCommentById = function(req,res){
-    
+
     var userId = req.params.userId;
-    Courses.aggregate([ { $match: {'posts.comments.author': mongoose.Types.ObjectId(userId)} },
-                        { $unwind: '$posts' },
-                        { $unwind: '$posts.comments' },
-                        { $match: {'posts.comments.author': mongoose.Types.ObjectId(userId)} }, // match again...
-                        { $project: {   _id: 0,
-                                        '_id': '$posts.comments._id', 
-                                        'course': { 
-                                            '_id': '$_id', 
-                                            'name': '$courseName' 
-                                         }, 
-                                        'post': { 
-                                            '_id': '$posts._id'
-                                         },
-                                        'content': '$posts.comments.content',
-                                        'upvotes': '$posts.comments.upvotes',
-                                        'updatedAt': '$posts.comments.updatedAt', 
-                                        'createdAt': '$posts.comments.createdAt' 
-                                    } },
-                        { $sort: { 'createdAt': -1 } } ], function(err, result) {
-                                if (err) {
-                                    res.status(400);
-                                    res.json(err);
-                                }
-                                else{
-                                    res.status(200);
-                                    res.json(result);
-                                }
-                            }
-    );
+    Courses.aggregate()
+        .match({ 'posts.comments.author': mongoose.Types.ObjectId(userId) })
+        .unwind('$posts')
+        .unwind('$posts.comments')
+        .match({ 'posts.comments.author': mongoose.Types.ObjectId(userId) }) // match again...
+        .project({  
+            _id: 0,
+            '_id': '$posts.comments._id', 
+            'course': { 
+                '_id': '$_id', 
+                'name': '$courseName' 
+             }, 
+            'post': { 
+                '_id': '$posts._id'
+             },
+            'content': '$posts.comments.content',
+            'upvotes': '$posts.comments.upvotes',
+            'updatedAt': '$posts.comments.updatedAt', 
+            'createdAt': '$posts.comments.createdAt' 
+        })
+        .sort({ 'createdAt': -1 })
+        .exec(function(err, result){
+            if (err) {
+                res.status(400);
+                res.json(err);
+            }
+            else{
+                res.status(200);
+                res.json(result);
+            }
+        });
 }
