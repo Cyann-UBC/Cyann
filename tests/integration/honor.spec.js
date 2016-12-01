ca30039945f0276262be4cad468f50088ca6068b
Justin Toh <tohjustin@hotmail.com>
2016-11-30 20:51:43 -0800

    Updated test cases to fit master branch API version

--- tests/integration/honor.spec.js
+++ tests/integration/honor.spec.js
@@ -5,58 +5,59 @@ const jwt = require('jsonwebtoken');
 var app = require('./../../server.js').app;
 const {populateUsers, clearUsers, users, user_tokens} = require('./../fixtures/fixtures-user.js');
 
+describe("<<<<<<<<<<<< HONOR API >>>>>>>>>>>>", () => {
+  describe("[API ROUTE] /api/honor/:userId", () => {
+    before(populateUsers);
+    after(clearUsers);
 
-describe("[API ROUTE] /api/honor/:userId", () => {
-  before(populateUsers);
-  after(clearUsers);
-
-  it('should return UNAUTHORIZED error (missing JWT)', (done) => {
-    request(app)
-      .get('/api/honor/'+users[0]._id)
-      .expect(401)
-      .expect((res) => {
-        expect(res.body).toInclude({
-          "message": "No authorization token was found",
-          "error": {
-            "name": "UnauthorizedError",
+    it('should return UNAUTHORIZED error (missing JWT)', (done) => {
+      request(app)
+        .get(`/api/honor/${users[0]._id}`)
+        .expect(401)
+        .expect((res) => {
+          expect(res.body).toInclude({
             "message": "No authorization token was found",
-            "code": "credentials_required",
-            "status": 401,
-            "inner": {
-              "message": "No authorization token was found"
+            "error": {
+              "name": "UnauthorizedError",
+              "message": "No authorization token was found",
+              "code": "credentials_required",
+              "status": 401,
+              "inner": {
+                "message": "No authorization token was found"
+              }
             }
-          }
+          })
         })
-      })
-      .end(done);
-  });
+        .end(done);
+    });
 
-  it('should return the user\'s honor points', (done) => {
-    request(app)
-      .get(`/api/honor/${users[0]._id}`)
-      .set('Authorization', `Bearer ${user_tokens[0]}`)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toInclude({
-          "message": users[0].name,
-          "honorPoints": users[0].honour,
-        });
-      })
-      .end(done);
-  });
+    it('should return the user\'s honor points', (done) => {
+      request(app)
+        .get(`/api/honor/${users[0]._id}`)
+        .set('Authorization', `Bearer ${user_tokens[0]}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            "userName": users[0].name,
+            "honorPoints": users[0].honour,
+          });
+        })
+        .end(done);
+    });
 
-  it('should NOT return the user\'s honor points (invalid :userId)', (done) => {
-    const invalidUserId = "0"
-    request(app)
-      .get(`/api/honor/${invalidUserId}`)
-      .set('Authorization', `Bearer ${user_tokens[0]}`)
-      .expect(400)
-      .expect((res) => {
-        expect(res.body).toInclude({
-          "message": "User not found",
-          "status" : 400,
-        });
-      })
-      .end(done);
+    it('should NOT return the user\'s honor points (invalid :userId)', (done) => {
+      const invalidUserId = "0"
+      request(app)
+        .get(`/api/honor/${invalidUserId}`)
+        .set('Authorization', `Bearer ${user_tokens[0]}`)
+        .expect(400)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            "message": "User not found",
+            "status" : 400,
+          });
+        })
+        .end(done);
+    });
   });
 });
