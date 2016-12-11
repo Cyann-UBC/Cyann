fd3e4775993639b305b079bd41ad16b03a431667
Howard-Zhou <howard12345678987654321@hotmail.com>
2016-12-10 22:57:45 -0800

    listens to port 443

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const hostname = 'cyann.tech';
+const port  = '80';
 var express = require("express");
 var morgan = require('morgan');                         // Log requests to the console (express4)
 var bodyParser = require("body-parser");                // Pull information from HTML POST (express4)
 var path = require("path");
 // var multer  = require('multer')                      // Use multer for file uploads
 var expressJWT = require('express-jwt');
+var helmet = require('helmet')
 var cors = require('cors');
-
+var https = require('https');
+var fs = require('fs');
+var options = {
+       	ca:fs.readFileSync(__dirname+'/cyann_tech.ca-bundle'),
+       	key: fs.readFileSync(__dirname+'/cyann_tech.key'),
+       	cert: fs.readFileSync(__dirname+'/cyann_tech.crt')
+};
+console.log(__dirname)
 //----------------------------------------
 // DATABASE
 //----------------------------------------
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
@@ -18,7 +27,12 @@ mongoose.Promise = global.Promise;
 //----------------------------------------
 var app = express();
 app.use(cors());
+app.use(express.static(__dirname));
 // app.use(morgan('dev'));                              // Log every request to the console{}
+
+app.use(helmet());
+// app.use(morgan('dev'));                              // Log every request to the console{} (for dev only)
+
 app.use(bodyParser.json());                             // Parse application/json
 app.use(bodyParser.urlencoded({"extended" : false}));   // DON'T parse application/x-www-form-urlencoded
 app.use(expressJWT({ secret: 'CPEN321_CYANN' }).unless({ path: ['/api/users/register'] })); //jwt verification middleware.
@@ -48,6 +62,8 @@ app.use(function(err, req, res, next) {
 //----------------------------------------
 // START SERVER
 //----------------------------------------
-app.listen(8080);
-console.log("Listening to PORT 8080");
+ https.createServer(options, function(req,res){
+     app.handle(req, res);
+ }).listen(443);
+ console.log("Listening to PORT 443");
 module.exports.app = app;
